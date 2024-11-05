2f514b3c5ecc8a058834fe7943767e24e3cc2c63
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-11-05 18:22:00 +0200

    update

--- core/View.php
+++ core/View.php
@@ -7,19 +7,4 @@ class View
 		extract($data);
 		require '../view/' . $view . '.php';
 	}
-
-	public static function exists($view)
-	{
-		return file_exists('../view/' . $view . '.php');
-	}
-
-	public static function first($views, $data)
-	{
-		foreach ($views as $view) {
-			if (self::exists($view)) {
-				self::render($view, $data);
-				return;
-			}
-		}
-	}
 }
